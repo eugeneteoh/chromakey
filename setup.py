0039265c6830e0c7f6f663c6b9c2fd3b58ca4b6b
Eugene Teoh <weichien.teoh@dyson.com>
2023-06-26 15:34:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 from setuptools import find_packages, setup
+from pathlib import Path
+
+# read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="chromakey",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=["Pillow", "scipy", "numpy"],
+    url="https://github.com/eugeneteoh/chromakey",
+    author="Eugene Teoh",
+    author_email="eugenetwc1@gmail.com",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type='text/markdown' 
 )
