4c138763f45bd56a4824ad3720b65162f5afbc0d
Eugene Teoh <50418168+eugeneteoh@users.noreply.github.com>
2024-07-14 16:15:14 +0100

    Bump version to v0.2.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="chromakey",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     install_requires=["Pillow", "numpy"],
     extras_require={
